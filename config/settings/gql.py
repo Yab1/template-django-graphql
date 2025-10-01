1964a8e1fd535e7f4e2d2c1511f04cd1edfa2efd
yeabsera <yeabseradeveloper@gmail.com>
2025-10-01 14:56:00 +0300

    Add default settings for Strawberry Django in GQL configuration
    Introduce new default settings for Strawberry Django, including options for mutations, pagination, and error handling. This enhances the GraphQL configuration for better functionality and usability.

--- config/settings/gql.py
+++ config/settings/gql.py
@@ -1,8 +1,16 @@
 from config.env import env
 
+# Default settings for Strawberry Django
+# https://strawberry.rocks/docs/django/guide/settings
+
 STRAWBERRY_DJANGO = {
     "FIELD_DESCRIPTION_FROM_HELP_TEXT": True,
     "TYPE_DESCRIPTION_FROM_MODEL_DOCSTRING": True,
+    "MUTATIONS_DEFAULT_ARGUMENT_NAME": "input",
+    "MUTATIONS_DEFAULT_HANDLE_DJANGO_ERRORS": True,
+    "GENERATE_ENUMS_FROM_CHOICES": True,
+    "MAP_AUTO_ID_AS_GLOBAL_ID": True,
+    "PAGINATION_DEFAULT_LIMIT": 10,
 }
 
 
