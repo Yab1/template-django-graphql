b35f304f50d2b5dd33308e58b8d0cd5cdfa41069
yeabsera <yeabseradeveloper@gmail.com>
2025-10-18 20:53:51 +0300

    Add authentication to create_grandparent mutation in FamilyMutation
    This commit updates the create_grandparent mutation in the FamilyMutation class to include the IsAuthenticated permission extension. This change ensures that only authenticated users can create GrandParent entities, enhancing the security of the Family app's GraphQL operations.

--- core/family/mutations.py
+++ core/family/mutations.py
@@ -1,5 +1,6 @@
 import strawberry
 from strawberry_django.mutations import create
+from strawberry_django.permissions import IsAuthenticated
 
 from core.family.inputs import GrandParentInput
 from core.family.types import GrandParentType
@@ -7,4 +8,8 @@ from core.family.types import GrandParentType
 
 @strawberry.type
 class FamilyMutation:
-    create_grandparent: GrandParentType = create(GrandParentInput)
+    create_grandparent: GrandParentType = create(
+        GrandParentInput,
+        extensions=[IsAuthenticated()],
+        # handle_django_errors=True,
+    )
