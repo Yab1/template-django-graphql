1ff431072ca47bf2c550e8b6ad32cf98477c7538
yeabsera <yeabseradeveloper@gmail.com>
2025-10-20 14:34:33 +0300

    Remove Family app and all associated files, including models, admin configurations, migrations, and GraphQL settings. This cleanup streamlines the codebase by eliminating unused components.

--- core/family/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import Child, GrandChild, GrandParent
-
-admin.site.register(GrandParent)
-admin.site.register(GrandChild)
-admin.site.register(Child)
